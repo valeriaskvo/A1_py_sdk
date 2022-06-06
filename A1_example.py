9fc37785902847d95572ba59ca42c0af0397e245
valriaskvo <valriaskvo@gmail.com>
2022-06-06 12:00:36 +0300

    Add a code for the sdk

---
+++ A1_example.py
@@ -0,0 +1,54 @@
+from A1_motor import A1_motor, open_serial_port
+from time import perf_counter
+from time import sleep
+import numpy as np
+
+serial_port = open_serial_port()
+ids = [0, 1, 2]
+pd = {"kp": 0.01, "kd": 0.7}
+
+motors = []
+for id in ids:
+    motors.append(A1_motor(id, serial_port, pd))
+
+t = 0
+t_max = 10
+dt = 0
+n = 0
+
+A = np.deg2rad(45)
+w = np.pi
+
+input("Press any key")
+t0 = perf_counter()
+
+try:
+    while t<t_max:
+        t1 = perf_counter()
+
+        q = A * np.sin(w*t)
+        dq = A * w * np.cos(w*t)
+        for motor in motors:
+            motor.set_q_and_dq(q, dq)
+            sleep(0.00001)
+
+        t2 = perf_counter()
+        
+        dt = dt + (t2 - t1)
+        n = n + 1
+
+        t = perf_counter() - t0
+   
+    print("Average run time:", dt/n,"sec")
+    print("Average frequency:", n/dt,"sec")
+
+except KeyboardInterrupt:
+    print("Motor stop by keyboard")
+
+except Exception as e:
+    print("Motor stop by inner exception")
+    print("Exception is", e.args[0])
+
+finally:
+    for motor in motors:
+        motor.stop()
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # A1_py_sdk
+Python SDK for motors unitree A1, see documentation [here](https://www.unitree.com/products/a1_motor)
+
+Do the following steps to install sdk on your computer:
+1. Remove all files from `build` directory
+2. Run the following comand in terminal:
+``` bash
+cd build
+cmake ..
+make
+```
